3553dab6bad4fca56c3495f65dca6d4e0dde668b
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-12-13 22:48:23 +0100

    bump version
    add information.java

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.parentpom;
 
 public class Information {
 	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-12-13T21:12:57Z";
-	public static final String pomVersion = "0.1.0";
+	public static final String buildTime = "2020-12-13T21:47:55Z";
+	public static final String pomVersion = "0.1.2";
 }
